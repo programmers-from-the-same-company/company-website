7efeb59632fc2281acd4937b324d516c3b09abb3
Gaurav Menezes <43903974+GauravMenezes-Dev@users.noreply.github.com>
2021-09-05 04:40:11 +0530

    Issue #5: Pending users appear before current in mobile and Spinner is present while data is loading. (#7)
    * Added a nice spinner and made sure pending comes first as well as search functionality
    
    * Removed redundant react-loader-spinner dependency and used bootstrap's own loading component
    
    * Added card wrapping and search functionality for users
    
    * Fixed yarn.lock and made small stylistic changes
    
    * Added icon
    
    Co-authored-by: Edward Marozzi <38032037+ted-marozzi@users.noreply.github.com>

--- src/Home/UsernameInput.tsx
+++ src/Home/UsernameInput.tsx
@@ -3,8 +3,10 @@ import React, { useEffect, useState } from 'react';
 function UsernameInput() {
     const [username, setUsername] = useState("");
     const [apiResponse, setApiResponse] = useState("");
-    const [currentMembers, setCurrentMembers] = useState([]);
-    const [pendingMembers, setPendingMembers] = useState([]);
+    const [currentMembers, setCurrentMembers] = useState<string[]>([]);
+    const [pendingMembers, setPendingMembers] = useState<string[]>([]);
+    const [search, setSearch] = useState("")
+    const [isLoading, setIsLoading] = useState(true)
 
     const apiEndpoint = "https://company-website-api.herokuapp.com/";
     const sendInvitation = async () => {
@@ -14,15 +16,22 @@ function UsernameInput() {
             method: "POST",
             body: JSON.stringify({ Login: username })
         }).then(
-            (res) => setApiResponse(
-                "Hey " + username + " here is your api response: " + res.status + " " + res.statusText)
+            (res) => {
+                const salutation = !username || username === "" ? "Hey" : "Hey " + username;
+                setApiResponse(
+                    salutation + ", here is your api response: " + res.status + " " + res.statusText)
+            }
         ).catch(
             (error) => console.error(error)
         );
     }
 
     const makeListItem = (member: string) => {
-        return <li key={member}>{member}</li>;
+        return <li key={member}>
+            <div className="card shadow-sm my-2">
+                <div className="card-body">{member}</div>
+            </div>
+        </li>;
     }
 
     const getMembers = async () => {
@@ -32,6 +41,7 @@ function UsernameInput() {
         }).then((json) => {
             setCurrentMembers(json.active.map(extractUsername));
             setPendingMembers(json.pending.map(extractUsername));
+            setIsLoading(false)
         }).catch((error) => console.error(error));
     }
 
@@ -40,6 +50,12 @@ function UsernameInput() {
         // Call api to get current members and pending members and set state
         getMembers();
     }, [apiResponse]);
+
+    const SpinnerComponent = (
+        <div className="spinner-grow" role="status"/>
+    )
+
+    const filterFunction = (member: string) => (search !== ""? member.toUpperCase().includes(search.toUpperCase()): true)
 
     return (
         <div className="container text-center">
@@ -60,21 +76,44 @@ function UsernameInput() {
                 Send Invitation
             </button>
 
+            
+
             <h3 className="mt-5">{apiResponse}</h3>
             <div className= "m-2">Please check for your name in the pending Invitations section after you have sent your invitation.</div>
             <div className= "m-2">Click this <a href="https://github.com/programmers-from-the-same-company">link</a> or check your email to accept.</div>
+
+            <div className="container my-4">
+                <h2>Search for a username</h2>
+                <input
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="form-control m-2 w-50 text-center mx-auto"
+                />
+            </div>
+            
             <div className="row m-5">
                 <div className="col text-center">
-                    <h2>Current Members</h2>
-                    <ul className="list-unstyled">
-                        {currentMembers.map(makeListItem)}
-                    </ul>
+                    <h2>Pending Invitations</h2>
+                    {
+                        isLoading ? SpinnerComponent : 
+                        (
+                            <ul className="list-unstyled">
+                                {pendingMembers.filter(filterFunction).map(makeListItem)}                     
+                            </ul> 
+                        )
+                    }
                 </div>
                 <div className="col text-center">
-                    <h2>Pending Invitations</h2>
-                    <ul className="list-unstyled">
-                        {pendingMembers.map(makeListItem)}
-                    </ul>
+                    <h2>Current Members</h2>
+                    {
+                        isLoading ? SpinnerComponent : 
+                        (
+                            <ul className="list-unstyled">
+                                {currentMembers.filter(filterFunction).map(makeListItem)}
+                            </ul>
+                        )
+                    }
                 </div>
             </div>
         </div>
