2087f75e2b4daa60e1296903cb35cc0a94fb1479
ted-marozzi <edwardmarozzi@gmail.com>
2022-02-19 15:17:58 +1100

    Added type safty to fix bug

--- src/Home/UsernameInput.tsx
+++ src/Home/UsernameInput.tsx
@@ -1,5 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import SpinnerComponent from '../Global/GlobalComponents';
+type Users = {
+    "active": User[] | null,
+    "pending": User[] | null
+}
+
+type User = {
+    "login": string | null
+}
 
 function UsernameInput() {
     const [username, setUsername] = useState("");
@@ -36,12 +44,13 @@ function UsernameInput() {
     }
 
     const getMembers = async () => {
-        const extractUsername = (user: any) => user.login;
+        const extractUsername = (user: User) => user.login ?? "";
+
         fetch(apiEndpoint + "users").then((res) => {
             return res.json();
-        }).then((json) => {
-            setCurrentMembers(json.active.map(extractUsername));
-            setPendingMembers(json.pending.map(extractUsername));
+        }).then((json: Users) => {
+            setCurrentMembers(json.active != null ? json.active.map(extractUsername) : []);
+            setPendingMembers(json.pending != null ? json.pending.map(extractUsername) : []);
             setIsLoading(false)
         }).catch((error) => console.error(error));
     }
@@ -53,7 +62,7 @@ function UsernameInput() {
     }, [apiResponse]);
 
 
-    const filterFunction = (member: string) => (search !== ""? member.toUpperCase().includes(search.toUpperCase()): true)
+    const filterFunction = (member: string) => (search !== "" ? member.toUpperCase().includes(search.toUpperCase()) : true)
 
     return (
         <div className="container text-center">
@@ -74,11 +83,11 @@ function UsernameInput() {
                 Send Invitation
             </button>
 
-            
+
 
             <h3 className="mt-5">{apiResponse}</h3>
-            <div className= "m-2">Please check for your name in the pending Invitations section after you have sent your invitation.</div>
-            <div className= "m-2">Click this <a href="https://github.com/programmers-from-the-same-company">link</a> or check your email to accept.</div>
+            <div className="m-2">Please check for your name in the pending Invitations section after you have sent your invitation.</div>
+            <div className="m-2">Click this <a href="https://github.com/programmers-from-the-same-company">link</a> or check your email to accept.</div>
 
             <div className="container my-4">
                 <h2>Search for a username</h2>
@@ -89,28 +98,28 @@ function UsernameInput() {
                     className="form-control m-2 w-50 text-center mx-auto"
                 />
             </div>
-            
+
             <div className="row m-5">
                 <div className="col text-center">
                     <h2>Pending Invitations</h2>
                     {
-                        isLoading ? SpinnerComponent : 
-                        (
-                            <ul className="list-unstyled">
-                                {pendingMembers.filter(filterFunction).map(makeListItem)}                     
-                            </ul> 
-                        )
+                        isLoading ? SpinnerComponent :
+                            (
+                                <ul className="list-unstyled">
+                                    {pendingMembers.filter(filterFunction).map(makeListItem)}
+                                </ul>
+                            )
                     }
                 </div>
                 <div className="col text-center">
                     <h2>Current Members</h2>
                     {
-                        isLoading ? SpinnerComponent : 
-                        (
-                            <ul className="list-unstyled">
-                                {currentMembers.filter(filterFunction).map(makeListItem)}
-                            </ul>
-                        )
+                        isLoading ? SpinnerComponent :
+                            (
+                                <ul className="list-unstyled">
+                                    {currentMembers.filter(filterFunction).map(makeListItem)}
+                                </ul>
+                            )
                     }
                 </div>
             </div>
